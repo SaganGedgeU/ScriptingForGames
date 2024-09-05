31f52dc181ac9e307305c856c7cbf03cdf035d2a
Sagan Gedge <11071435@UVU.EDU>
2024-09-05 10:53:29 -0600

    Module 2 challenge
    Added more Childs moving at the diagonal

--- GameObjectsAndTransformLab/Assets/scripts/TransformController.cs
+++ GameObjectsAndTransformLab/Assets/scripts/TransformController.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class TransformController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject targetchildOne;
+    public GameObject targetchildTwo;
+    public GameObject targetchildthree;
+    public GameObject targetchildFour;
+    public GameObject Diagone;
+    public GameObject Diagtwo;
+    public GameObject Diagthree;
+    public GameObject Diagfour;
     void Start()
     {
         
@@ -13,8 +20,28 @@ public class TransformController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        MoveSheres();
+
+
+    }
+
+    void MoveSheres() {
+
         var x = Mathf.PingPong(t: Time.time, length: 3);
         var p = new Vector3(x: 0, x, z: 0);
-        transform.position = p;
+        var q = new Vector3(x: x, 0, z: 0);
+        var u = new Vector3(x: x, x, z: 0);
+        var v = new Vector3(x: x, -x, z: 0);
+
+
+        targetchildOne.transform.position = p;
+        targetchildTwo.transform.position = -p;
+        targetchildthree.transform.position = q;
+        targetchildFour.transform.position = -q;
+        Diagone.transform.position = u;
+        Diagtwo.transform.position = -u;
+        Diagthree.transform.position = v;
+        Diagfour.transform.position = -v;
     }
 }
