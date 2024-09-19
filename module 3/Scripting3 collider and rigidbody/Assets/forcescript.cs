494d5ac668bdd6c83cf5c9f35d67ee9f34359697
Sagan Gedge <11071435@UVU.EDU>
2024-09-19 09:30:21 -0600

    Added color change and bounce to scripting 3 challenge

--- module 3/Scripting3 collider and rigidbody/Assets/forcescript.cs
+++ module 3/Scripting3 collider and rigidbody/Assets/forcescript.cs
@@ -5,23 +5,22 @@ using UnityEngine;
 public class forcescript : MonoBehaviour
 {
     private Rigidbody rb;
+    public float bounceForce = 6f;
+    public Color newColor = Color.red;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.AddForce(Vector3.left * 500);
-
+       // rb.AddForce(Vector3.left * 500);
     }
 
     void OnCollisionEnter(Collision collisio)
     {
         Debug.Log("collsion with" + collisio.gameObject.name);
-        
-            rb.AddForce(Vector3.left * 500);
-        
-    }
-    // Update is called once per frame
-    void Update()
-    {
+        GetComponent<Renderer>().material.color = newColor;
+
+            rb.AddForce(Vector3.up * bounceForce, ForceMode.Impulse);
         
     }
+   
 }
