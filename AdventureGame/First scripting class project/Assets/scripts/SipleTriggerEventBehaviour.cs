fef5fe630406fb5441c11ecabe3708d4dd917f73
Sagan Gedge <11071435@UVU.EDU>
2024-10-29 18:50:47 -0600

    Scripting lab 7
    I had some problems and messaged you and you said just to do the challenge, but the challenge is to do the same thing for another value, like health or mana, so I figured out my own way to do the health bar. I know its not very scalable, so consider it a temporary solution.

--- AdventureGame/First scripting class project/Assets/scripts/SipleTriggerEventBehaviour.cs
+++ AdventureGame/First scripting class project/Assets/scripts/SipleTriggerEventBehaviour.cs
@@ -2,20 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 public class SipleTriggerEventBehaviour : MonoBehaviour
 {
    public Animator animator;
-    
+    public Slider helthbar;
+    public float Player_health;
     public UnityEvent triggerEvent;
 
     void start() {
-      //  animator = GetComponent<Animator>();
+        //  animator = GetComponent<Animator>();
+       // Player_health = 50;
     }
-   
+    void Update() {
+        helthbar.value = Player_health;
+    }
     private void OnTriggerEnter(Collider other) 
     {
         triggerEvent.Invoke();
         animator.SetTrigger("HitTrigger");
         Debug.Log("player touched the spike!");
+
+          Player_health = Player_health - .25f;
+            Debug.Log(Player_health);
+        
+      //  helthbar
     }
 }
