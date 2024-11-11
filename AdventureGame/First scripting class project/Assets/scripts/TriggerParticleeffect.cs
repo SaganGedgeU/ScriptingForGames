65406913ba5d8c403ba1f3be1cab564405ba5f35
Sagan Gedge <11071435@UVU.EDU>
2024-11-10 20:48:21 -0700

    Challange10

--- AdventureGame/First scripting class project/Assets/scripts/TriggerParticleeffect.cs
+++ AdventureGame/First scripting class project/Assets/scripts/TriggerParticleeffect.cs
@@ -1,21 +1,39 @@
 using UnityEngine;
+using System.Collections;
 
 [RequireComponent(typeof(ParticleSystem), typeof(Collider))]
 public class TriggerParticleEffect : MonoBehaviour
 {
-    private ParticleSystem newParticleSystem; // Reference to the Particle System
-    public int particleAmount = 10; // Exposed variable for particle amount
+    private ParticleSystem particle_System; // Reference to the Particle System
+    public int FirstEmisionAmount = 10; // Exposed variable for particle amount
+    public int SecondEmisionAmount = 20;
+    public int ThirdEmisionAmount = 30;
+    public float delayBetweemEmissions = 0.5f;
+
+
 
     private void Start()
     {
-        newParticleSystem = GetComponent<ParticleSystem>();
+        particle_System = GetComponent<ParticleSystem>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<CharacterController>()) // Check if the player triggered the event
         {
-            newParticleSystem.Emit(particleAmount); // Emit the specified amount of particles
+            StartCoroutine(EmitParticlesCoroutine());
         }
+    }
+
+    private IEnumerator EmitParticlesCoroutine()
+    {
+        particle_System.Emit(FirstEmisionAmount);
+        yield return new WaitForSeconds(delayBetweemEmissions);// Emit the specified amount of particles
+
+        particle_System.Emit(SecondEmisionAmount);
+        yield return new WaitForSeconds(delayBetweemEmissions);
+
+        particle_System.Emit(ThirdEmisionAmount);
+
     }
 }
